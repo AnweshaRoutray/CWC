b4b7cfb1edb858ee4a32dcd50890887e9287ca13
Anwesha Routray <54020267+AnweshaRoutray@users.noreply.github.com>
2020-11-03 21:21:06 +0530

    Update q1.java

--- src/q1.java
+++ src/q1.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
+import java.lang.String;
 public class q1 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Input: ");
 		String str=sc.nextLine();
 		System.out.print("Output: ");
-		int ic=0; // to check if the input is valid or invalid
-		for(int i=0;i<str.length();i++)//checking for invalid input
+		String str2="";
+		 // to check if the input is valid or invalid
+		
+		for(int i=0;i<str.length();i++)
 		{
 			char ch=str.charAt(i);
 			if(!(ch=='A'||ch=='C'||ch=='G'||ch=='T'))
 			{
-				System.out.println("Invalid Input");
-				ic++;
+				str2="Invalid Input";
 				break;
 			}	
-		}
-		for(int i=0;i<str.length();i++)
-		{
-			char ch=str.charAt(i);
-			if((ch=='A'||ch=='C'||ch=='G'||ch=='T') && ic==0)
+			else
 			{
 			   if(ch=='G')
-				System.out.print("C");
+				str2=str2.concat("C");
 			   else if(ch=='C')
-				System.out.print("G");
+				str2=str2.concat("G");
 			   else if(ch=='T')
-				System.out.print("A");
+				str2=str2.concat("A");
 			   else if(ch=='A')
-				System.out.print("U");
-			}
-		}	
+				str2=str2.concat("U");
+		  }
+		}
+		System.out.print(str2);
 	}
 }

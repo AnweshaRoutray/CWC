93c7d77d84ec7521da601e8def90f3f1d4c04d89
Anwesha Routray <54020267+AnweshaRoutray@users.noreply.github.com>
2020-11-03 21:24:22 +0530

    Update q6.java

--- src/q6.java
+++ src/q6.java
@@ -1,25 +1,24 @@
-
 public class q6 {
 
 	public static void main(String[] args) {
-		int n=2;
-		for(int ct=1;ct<=10001;)
+		int n=3;
+		for(int ct=1;ct<=10000;)
 		{
 			int ct2=0;
-			for(int i=1;i<=(int)(n/2);i++)
+			for(int i=2;i<=(int)Math.sqrt(n);i++)
 			{
 				if(n%i==0)
 					ct2++;
 			}
-			if(ct2==1)
+			if(ct2==0)
 			{
 				ct++;
 			}
 			ct2=0;
-			n++;
+			n=n+2;
 
 		}
-		System.out.println("The 10001st prime number is "+(n-1));
+		System.out.println("The 10001st prime number is "+(n-2));
 	}
 
 }
